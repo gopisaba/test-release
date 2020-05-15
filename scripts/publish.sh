0b1bcb9dbab8c57ca84b2d2e2a87a6401db6f127
Gopi <gopisaba@users.noreply.github.com>
2020-05-15 20:36:17 +0100

    fix: Update the download url

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,4 +8,4 @@ tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
 # Publish
 sed -i "s/version: .*/version: $version/g" anotherchart/Chart.yaml
 linux-amd64/helm package --version "$version" anotherchart/.
-linux-amd64/helm repo index --url https://gopisaba.github.io/test-release/ --merge index.yaml .
+linux-amd64/helm repo index --url "https://github.com/gopisaba/test-release/releases/download/v${version}/" --merge index.yaml .
