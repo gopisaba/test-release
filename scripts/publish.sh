63d21158acd6f69b3e15b57cb303e5ea6c574c49
Gopi <gopisaba@users.noreply.github.com>
2020-05-15 18:36:28 +0100

    feat: run from bash script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-# Install Helm
+#!/bin/bash -eou
+
+# Variables
+version=$1
+
+# Install Helm client
 wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
-# Update version
-version="${nextRelease.version}"
-sed -i "s/version: .*/version: $version/g" anotherchart/Chart.yaml
-# Package
-linux-amd64/helm package --version $version anotherchart/.
+
 # Publish
-linux-amd64/helm repo index --url https://gopisaba.github.io/test-release/ --merge index.yaml anotherchart/.
+sed -i "s/version: .*/version: $version/g" anotherchart/Chart.yaml
+linux-amd64/helm package --version "$version" anotherchart/.
+linux-amd64/helm repo index --url https://gopisaba.github.io/test-release/ --merge index.yaml .
