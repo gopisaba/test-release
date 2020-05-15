974d1dcf5cfa82eed7589e0d7156add91dbcc3d0
Gopi <gopisaba@users.noreply.github.com>
2020-05-15 17:53:49 +0100

    fix: run the scripts/publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,8 +3,9 @@
 wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.2.1-linux-amd64.tar.gz
 # Update version
-sed -i "s/version: .*/version: ${nextRelease.version}/g" anotherchart/Chart.yaml
+version="${nextRelease.version}"
+sed -i "s/version: .*/version: $version/g" anotherchart/Chart.yaml
 # Package
-linux-amd64/helm package --version ${nextRelease.version} anotherchart/.
+linux-amd64/helm package --version $version anotherchart/.
 # Publish
 linux-amd64/helm repo index --url https://gopisaba.github.io/test-release/ --merge index.yaml anotherchart/.
